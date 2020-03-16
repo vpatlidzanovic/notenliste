b2ada025e7f3fb1eff219e2504404ea55bc72f91
vpatlidzanovic <vpatlidzanovic@student.tgm.ac.at>
2020-03-16 17:04:19 +0100

    Initial commit

---
+++ src/vpatlidzanovic/Emailliste.java
@@ -0,0 +1,101 @@
+/**
+ * 
+ */
+package vpatlidzanovic;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeMap;
+
+/**
+ * Speichert alle Emails-Adressen eines Schülers.
+ * @author Viktor Patlidzanovic
+ *
+ */
+public class Emailliste {
+	private TreeMap<Schüler, HashSet<String>> emails;
+	
+	/**
+	 * Initialisiert das Attribut emails.
+	 */
+	public Emailliste() {
+		this.emails = new TreeMap<>();
+	}
+	
+	/**
+	 * Fügt eine Email der bereits bestehenden Liste eines Schülers hinzu.
+	 * Falls noch kein Eintrag dieses Schülers vorhanden ist wird ein neuer erstellt.
+	 * @param s der Schüler, dem die Email gespeicher werden soll
+	 * @param email die Email, die gespeichert werden soll
+	 */
+	void addEmail(Schüler s, String email) {
+		if (this.emails.get(s) == null) {
+			HashSet<String> set = new HashSet<>();
+			set.add(email);
+			this.emails.put(s, set);
+		} else {
+			HashSet<String> set = this.emails.get(s);
+			set.add(email);
+			this.emails.put(s, set);
+		}
+	}
+	
+	/**
+	 * Gibt ein String-Array mit allen Emails des übergebenen Schülers zurück.
+	 * @param s der Schüler, dessen Emails verwendet werden sollen
+	 * @return
+	 */
+	public String[] searchEmail(Schüler s) {
+		String[] cache = null;
+		if (this.emails.get(s) != null) {
+			HashSet<String> set = this.emails.get(s);
+			cache = new String[set.size()];
+			set.toArray(cache);
+		}
+		return cache;
+	}
+	
+	/**
+	 * Löscht den Eintrag eines Schülers aus dem Attribut emails.
+	 * @param s
+	 */
+	public void removeSchüler(Schüler s) {
+		if (this.emails.get(s) != null) this.emails.remove(s);
+	}
+	
+	/**
+	 * Löscht eine Email eines Schülers aus dem Attribut emails.
+	 * @param s der Schüler, dessen Email gelöscht werden soll
+	 * @param email die Email, die gelöscht werden soll
+	 */
+	public void removeEmail(Schüler s, String email) {
+		if (this.emails.get(s) != null) {
+			HashSet<String> set = this.emails.get(s);
+			set.remove(email);
+			this.emails.put(s, set);
+		}
+	}
+	
+	/**
+	 * Wandelt das Attribut emails in ein String um. In diesem werden
+	 * die Schülernamen und -Nachnamen gefolgt von ihren Emails
+	 * zurückgegeben.
+	 */
+	public String toString() {
+		StringBuilder cache = new StringBuilder();
+		Set<Schüler> set = emails.keySet();
+		for (Schüler s: set) {
+			cache.append(s.getVorname() + " " + s.getNachname() + ": ");
+			String[] emails = searchEmail(s);
+			for (int i = 0; i < emails.length; i++) {
+				if (i != emails.length - 1) {
+					cache.append(emails[i] + ", ");
+				} else {
+					cache.append(emails[i]);
+				}
+			}
+			cache.append("\n");
+		}
+		return cache.toString();
+	}
+}
---
+++ src/vpatlidzanovic/Test.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package vpatlidzanovic;
+
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+/**
+ * Testet das Programm.
+ * @author Viktor Patlidzanovic
+ *
+ */
+public class Test {
+
+	/**
+	 * Testet das Programm.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//Testet Klasse Schüler
+		System.out.println("Klasse Schüler:");
+		SortedSet<Schüler> s = new TreeSet<>();
+		s.add(new Schüler(19, "Viktor", "Patlidzanovic", "B"));
+		
+		System.out.println("Equals:");
+		System.out.println(s.first().equals(new Schüler(19, "Viktor", "Patlidzanovic", "B")));
+		
+		System.out.println("\nHashCode:");
+		System.out.println(s.first().hashCode());
+		
+		System.out.println("\nCompareTo:");
+		System.out.println(s.first().compareTo(new Schüler(20, "Viktor", "Patlidzanovic", "B")));
+		
+		
+		System.out.println("\n--------------\n");
+		
+		
+		//Testet Klasse Emailliste
+		System.out.println("Klasse Emailliste:");
+		Emailliste e = new Emailliste();
+		Schüler viktor = new Schüler(19, "Viktor", "Patlidzanovic", "B");
+		e.addEmail(viktor, "vpatlidzanovic@student.tgm.ac.at");
+		e.addEmail(viktor, "max.mustermann@gmail.com");
+		
+		System.out.println("Hinzugefügte Emails:");
+		System.out.println(e.toString());
+		
+		e.removeEmail(viktor, "max.mustermann@gmail.com");
+		System.out.println("\nLöschen von Email max.mustermann@gmail.com:");
+		System.out.println(e.toString());
+		
+		e.removeSchüler(viktor);
+		System.out.println("\nLöschen von Schüler Viktor:");
+		System.out.println(e.toString());
+	}
+
+}
